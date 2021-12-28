cd639fea23281d4d932dcb5678ec93cb5f1d0b0b
Kaden Hansen <kadendeanhansen@gmail.com>
2021-12-28 16:56:21 -0500

    game speed scaling added and start game function

--- ground.js
+++ ground.js
@@ -1,7 +1,6 @@
 import { getAssetProp, incrementAssetProp, setAssetProp } from "./updateLevel.js"
 
 let ground = document.querySelectorAll(".ground")
-let ninja = document.querySelector(".ninja")
 let speed = .05
 
 
@@ -10,9 +9,9 @@ export function layGround() {
     setAssetProp(ground[1], "--left", 100)
 }
 
-export function moveGround(currentTime) {
+export function moveGround(currentTime, gameSpeed) {
     ground.forEach(groundSection => {
-        incrementAssetProp(groundSection, "--left", currentTime * speed * -1)
+        incrementAssetProp(groundSection, "--left", currentTime * gameSpeed * speed * -1)
 
         if (getAssetProp(groundSection, "--left") <= -100) {
             incrementAssetProp(groundSection, "--left", 200)
--- script.js
+++ script.js
@@ -2,10 +2,13 @@ import { layGround, moveGround } from "./ground.js"
 
 setWorldScale()
 window.addEventListener("resize", setWorldScale)
-
-layGround()
+document.addEventListener("keydown", startGame, {once: true})
 
 let previousTime
+let gameSpeed
+let gameRateIncrease = .00001
+let startScreen = document.querySelector(".start-screen")
+
 function update(time) {
     if (previousTime == null) {
         previousTime = time
@@ -13,12 +16,24 @@ function update(time) {
         return
     }
     let currentTime = time - previousTime
-    moveGround(currentTime)    
-
+    moveGround(currentTime, gameSpeed)
+    increaseGameSpeed(currentTime)
+    
     previousTime = time
     window.requestAnimationFrame(update)
 }
-window.requestAnimationFrame(update)
+
+function increaseGameSpeed(currentTime) {
+    gameSpeed += currentTime * gameRateIncrease
+}
+
+function startGame() {
+    previousTime = null
+    layGround()
+    gameSpeed = .75
+    startScreen.remove()
+    window.requestAnimationFrame(update)
+}
 
 function setWorldScale() {
     let worldScale
